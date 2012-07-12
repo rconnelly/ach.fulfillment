51d0df416965eb028b48cff3ec3d386b5d2f6bfe
Dmitriy Samoylov <dmitriy.samoylov@gmail.com>
2012-07-12 19:38:21 +0300

    additional placeholders

--- Build/SetupParams/Default/Web/ps_ach_service_update.ps1
+++ Build/SetupParams/Default/Web/ps_ach_service_update.ps1
@@ -3,16 +3,16 @@ $subID = "e8636035-1760-468b-9d18-324a989af982"
 $subName = "Windows Azure MSDN - Visual Studio Ultimate"
 $thumbprint = "D73E3662A5C781CF8194A121872E1999AA4C6109"
 $package = ".\Web\Ach.Fulfillment.Web.Azure.cspkg"
-$config = ".\Web\ServiceConfiguration.Cloud.cscfg"
+$config = ".\Web\ServiceConfiguration.${CloudTargetProfile}.cscfg"
 $storage = "portalvhdsxmg86s9pngrcg"
 $slot = "Staging"
-$label = "Stagev2.6"
+$label = "Stagev${ProductVersion}"
 
 #$cert = Get-Item cert:\\CurrentUser\My\$thumbprint 
 $cert = Get-AzureCertificate -ServiceName $service
 
-#Set-AzureSubscription –SubscriptionName $subName -SubscriptionId $subID -Certificate $cert -CurrentStorageAccount $storage
-Set-AzureSubscription –SubscriptionName $subName -SubscriptionId $subID -CurrentStorageAccount $storage
+#Set-AzureSubscription -SubscriptionName $subName -SubscriptionId $subID -Certificate $cert -CurrentStorageAccount $storage
+Set-AzureSubscription -SubscriptionName $subName -SubscriptionId $subID -CurrentStorageAccount $storage
 
 Set-AzureDeployment -Upgrade -ServiceName $service –Package $package -Configuration $config -Slot $slot -Label $label
 
