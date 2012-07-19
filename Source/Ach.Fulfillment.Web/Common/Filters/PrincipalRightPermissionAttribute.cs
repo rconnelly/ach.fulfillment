dc0c347f432dcddcfbee8003d6f9c00baefd6c6d
Dmitriy Samoylov <dmitriy.samoylov@gmail.com>
2012-07-19 19:31:20 +0300

    mvc 4 version of permission attribute

--- Source/Ach.Fulfillment.Web/Common/Filters/PrincipalRightPermissionAttribute.cs
+++ Source/Ach.Fulfillment.Web/Common/Filters/PrincipalRightPermissionAttribute.cs
@@ -1,103 +1,68 @@
 ﻿namespace Ach.Fulfillment.Web.Common.Filters
 {
     using System;
+    using System.Diagnostics.Contracts;
+    using System.Linq;
+    using System.Security.Principal;
     using System.Web;
     using System.Web.Mvc;
-    using System.Web.Security;
 
     using Ach.Fulfillment.Common.Security;
-    using Ach.Fulfillment.Common.Utils;
     using Ach.Fulfillment.Data;
 
-    using global::Common.Logging;
-
-    using Microsoft.Practices.ServiceLocation;
-
-    // TODO: why not authorization attribute
-    public class PrincipalRightPermissionAttribute : ActionFilterAttribute
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = true)]
+    public class PrincipalRightPermissionAttribute : AuthorizeAttribute
     {
         #region Constants and Fields
 
-        private static readonly ILog Log = LogManager.GetCurrentClassLogger();
+        private string[] accessRights;
 
         #endregion
 
         #region Constructors and Destructors
 
-        // TODO: we do not need add. add it is just several attributes
-        public PrincipalRightPermissionAttribute(params AccessRight[] allowRightOr)
+        public PrincipalRightPermissionAttribute(params AccessRight[] accessRightOr)
         {
-            this.AllowRightsOr = allowRightOr;
+            this.AccessRights = accessRightOr;
         }
 
         #endregion
 
         #region Public Properties
 
-        public AccessRight[] AllowRightsAnd { get; set; }
+        public AccessRight[] AccessRights
+        {
+            get
+            {
+                return
+                    (from accessRight in this.accessRights
+                     select (AccessRight)Enum.Parse(typeof(AccessRight), accessRight)).ToArray();
+            }
 
-        public AccessRight[] AllowRightsOr { get; set; }
+            set
+            {
+                this.accessRights =
+                    (from accessRight in value ?? new AccessRight[0] select accessRight.ToString("G")).ToArray();
+            }
+        }
 
         #endregion
 
-        #region Public Methods and Operators
+        #region Methods
 
-        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
         {
-            if (!filterContext.HttpContext.User.Identity.IsAuthenticated)
-            {
-                // in general should not access this code since we control access by web.config
-                Log.Fatal("Unauthenticated access to the restricted controller action has been occured.");
+            Contract.Assert(httpContext != null);
+            return base.AuthorizeCore(httpContext) && this.HasPermissions(httpContext.User);
+        }
 
-                // use the current url for the redirect
-                var link = filterContext.HttpContext.Request.Url != null
-                               ? filterContext.HttpContext.Request.Url.AbsolutePath
-                               : string.Empty;
-                
-                // send them off to the login page
-                var loginUrl = "{0}?ReturnUrl={1}".Fmt(FormsAuthentication.LoginUrl, link);
-                filterContext.HttpContext.Response.Redirect(loginUrl, true);
-            }
-            else
-            {
-                var principal = ServiceLocator.Current.GetInstance<IApplicationPrincipal>();
-
-                var allowAccess = false;
-
-                // check OR rights
-                if (this.AllowRightsOr != null) 
-                {
-                    foreach (var systemRight in this.AllowRightsOr)
-                    {
-                        var right = systemRight;
-                        allowAccess = principal.HasPermission(right.ToString());
-                        if (allowAccess)
-                        {
-                            break;
-                        }
-                    }
-                }
-
-                // check AND rights
-                else if (this.AllowRightsAnd != null) 
-                {
-                    foreach (var systemRight in this.AllowRightsAnd)
-                    {
-                        var right = systemRight;
-                        allowAccess = principal.HasPermission(right.ToString());
-                        if (!allowAccess)
-                        {
-                            break;
-                        }
-                    }
-                }
-
-                if (!allowAccess)
-                {
-                    // don't want to show user that page exists - thus throw 404 instead of access deny
-                    throw new HttpException(404, "Requested page was not found", new UnauthorizedAccessException("You are not authorized to view this page"));
-                }
-            }
+        private bool HasPermissions(IPrincipal user)
+        {
+            var principal = user as IApplicationPrincipal;
+            var isEmptyRightsList = this.accessRights == null || this.accessRights.Length == 0;
+            var hasPermissions = isEmptyRightsList
+                                 || (principal != null && this.accessRights.Any(principal.HasPermission));
+            return hasPermissions;
         }
 
         #endregion
