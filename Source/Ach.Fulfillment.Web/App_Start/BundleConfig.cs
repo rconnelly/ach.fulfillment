d579e51ff8232399dab03b8051b5c8c6a24d1184
Dmitriy Samoylov <dmitriy.samoylov@gmail.com>
2012-07-20 12:22:40 +0300

    bootstrap integration

--- Source/Ach.Fulfillment.Web/App_Start/BundleConfig.cs
+++ Source/Ach.Fulfillment.Web/App_Start/BundleConfig.cs
@@ -37,7 +37,8 @@
                 "~/Content/themes/base/jquery.ui.theme.css"));
 
             bundles.Add(new StyleBundle("~/Content/bootstrap/css").Include(
-                "~/Content/bootstrap.css"));
+                "~/Content/bootstrap.css",
+                "~/Content/bootstrap-responsive.css"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                 "~/Content/site.css"));
