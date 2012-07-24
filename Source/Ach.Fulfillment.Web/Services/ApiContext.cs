a164b777854a0b2607f7b85f8fe5b87a51b82c67
svechin <anatoliy.svechin@gmail.com>
2012-07-24 22:28:00 +0300

    initial oData prototype

---
+++ Source/Ach.Fulfillment.Web/Services/ApiContext.cs
@@ -0,0 +1,84 @@
+namespace Ach.Fulfillment.Web.Services
+{
+    using System;
+    using System.Linq;
+
+    using Ach.Fulfillment.Business;
+    using Ach.Fulfillment.Common;
+    using Ach.Fulfillment.Data.Specifications;
+    using Ach.Fulfillment.Web.Areas.Manage.Models;
+
+    using Microsoft.Practices.Unity;
+
+    using global::Common.Logging;
+
+    public class ApiContext : IDisposable
+    {
+        #region Constants and Fields
+
+        private IQueryable<UserGridModel> users;
+
+        private readonly UnitOfWork unitOfWork;
+
+        private static readonly ILog Log = LogManager.GetCurrentClassLogger(); 
+
+        #endregion
+
+        public ApiContext ()
+        {
+            Log.Debug("Creating ApiContext instance");
+
+            unitOfWork = new UnitOfWork();
+        }
+
+        #region Public Properties
+
+        [Dependency]
+        public IUserManager UserManager { get; set; }
+
+        public IQueryable<UserGridModel> Users
+        {
+            get
+            {
+                if (this.users == null)
+                {
+                    var list = this.UserManager.FindAll(new UserPaged(5, 10));
+                    this.users = (from u in list
+                                  select
+                                      new UserGridModel
+                                          {
+                                              Id = (int)u.Id,
+                                              Name = u.Name,
+                                              Email = u.Email,
+                                              Login =
+                                                  u.UserPasswordCredential != null
+                                                      ? u.UserPasswordCredential.Login
+                                                      : string.Empty
+                                          }).AsQueryable();
+                }
+
+                return this.users;
+            }
+        }
+
+        #endregion
+
+        public void Dispose()
+        {
+            Log.Debug("Disposing ApiContext");
+
+            this.Dispose(true);
+        }
+
+        protected virtual void Dispose (bool disposing)
+        {
+            if(disposing)
+            {
+                if(this.unitOfWork != null)
+                {
+                    this.unitOfWork.Dispose();
+                }
+            }
+        }
+    }
+}
---
+++ Source/Ach.Fulfillment.Web/Services/ClientApi.svc.cs
@@ -0,0 +1,44 @@
+﻿using System.Data.Services;
+using System.Data.Services.Common;
+
+namespace Ach.Fulfillment.Web.Services
+{
+    using System.Diagnostics.Contracts;
+    using System.ServiceModel;
+
+    using Ach.Fulfillment.Common.Exceptions;
+    using Ach.Fulfillment.Web.Configuration;
+
+    using Microsoft.Practices.ServiceLocation;
+    using Microsoft.Practices.Unity;
+
+    [ServiceBehavior(IncludeExceptionDetailInFaults = true)]
+    public class ClientApi : DataService<ApiContext>
+    {
+        protected override ApiContext CreateDataSource()
+        {
+            var context = base.CreateDataSource();
+
+            var container = ServiceLocator.Current.GetInstance<IUnityContainer>();
+            container.BuildUp(context);
+
+            return context;
+        }
+
+        public static void InitializeService(DataServiceConfiguration config)
+        {
+            config.SetEntitySetAccessRule("Users", EntitySetRights.AllRead);
+            config.DataServiceBehavior.MaxProtocolVersion = DataServiceProtocolVersion.V2;
+        }
+
+        protected override void HandleException(HandleExceptionArgs args)
+        {
+            Contract.Assert(args != null);
+            Contract.Assert(args.Exception != null);
+
+            var handledException = args.Exception.TransformException(WebContainerExtension.DefaultPolicy);
+
+            throw handledException;
+        }
+    }
+}
