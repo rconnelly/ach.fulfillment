1445fd71bdfe008e4f3d209a7955125016daabe8
Gusynina <gusynina@gmail.com>
2012-10-25 01:45:35 +0300

    Add ftp uploading

--- Source/Ach.Fulfillment.Scheduler/Jobs/GenerateAchFilesJob.cs
+++ Source/Ach.Fulfillment.Scheduler/Jobs/GenerateAchFilesJob.cs
@@ -1,6 +1,9 @@
 ﻿namespace Ach.Fulfillment.Scheduler.Jobs
 {
     using System;
+    using System.IO;
+
+    using Ach.Fulfillment.Scheduler.Common;
     using Business;
     using Fulfillment.Common;
 
@@ -29,6 +32,29 @@
                     var dataMap = context.JobDetail.JobDataMap;
                     var achfilesStore = dataMap.GetString("AchFilesStore");
                     this.Manager.Generate(achfilesStore);
+
+                    var ftphost = dataMap.GetString("FtpHost");
+                    var ftpfilepath = dataMap.GetString("FtpFilePath");
+                    var ftpfullpath = "ftp://" + ftphost + ftpfilepath;
+
+                    var userId = dataMap.GetString("UserId");
+                    var password = dataMap.GetString("Password");
+
+                    if (!(string.IsNullOrEmpty(userId) && !string.IsNullOrEmpty(password)))
+                    {
+                        var directory = new DirectoryInfo(achfilesStore);
+                        var files = directory.GetFiles("*.ach");
+                        var ftpUploader = new FtpHelpers();
+
+                        foreach (var file in files)
+                        {
+                            var destinationPath = ftpfullpath + "/" + file.Name;
+                            var sourcePath = achfilesStore + "/" + file.Name;
+
+                            // this.Uploadfiles(destinationPath, sourcePath, userId, password);
+                            ftpUploader.AsynchronousUpload(destinationPath, sourcePath, userId, password);
+                        }
+                    }
                 }
             }
             catch (Exception ex)
@@ -38,5 +64,43 @@
         }
 
         #endregion
+
+        #region Private Methods
+
+/*
+        private void Uploadfiles(string ftpfullpath, string sourceDirectory, string userId, string password)
+        {
+            try
+                {
+                    var ftpRequest = (FtpWebRequest)WebRequest.Create(ftpfullpath);
+                    ftpRequest.Timeout = 10000;
+                    ftpRequest.ReadWriteTimeout = 10000;
+                    ftpRequest.KeepAlive = true;
+                    ftpRequest.UseBinary = true;
+                    ftpRequest.Method = WebRequestMethods.Ftp.UploadFile;
+                    ftpRequest.Credentials = new NetworkCredential(userId, password);
+
+                    var fileStream = File.OpenRead(sourceDirectory);
+                    var fileContents = new byte[fileStream.Length];
+                    fileStream.Read(fileContents, 0, fileContents.Length);
+                    fileStream.Close();
+
+                    ftpRequest.ContentLength = fileContents.Length;
+
+                    var ftpstream = ftpRequest.GetRequestStream();
+                    ftpstream.Write(fileContents, 0, fileContents.Length);
+                    ftpstream.Close();
+
+                   var response = (FtpWebResponse)ftpRequest.GetResponse();
+                   response.Close();
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("\r\n- Problem uploading file. url = " + sourceDirectory + ", Msg = " + ex.Message + ", " + ex.StackTrace);
+                }
+            }
+*/
+        }
+
+        #endregion
     }
-}
